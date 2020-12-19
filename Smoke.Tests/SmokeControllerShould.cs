7d4abe650e72e41b46754d162570af6f3d433273
tpierrain <thomas@pierrain.net>
2020-12-19 13:02:30 +0100

    Check something related to the test execution

--- Smoke.Tests/SmokeControllerShould.cs
+++ Smoke.Tests/SmokeControllerShould.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
@@ -37,11 +38,33 @@ namespace Smoke.Tests
             SmokeTestSessionResult smokeTestResult = null;
 
             var acceptableDeltaInMsec = 700;
+
             Check.ThatAsyncCode(async () =>
             {
                 smokeTestResult = await controller.RunSmokeTests();
             }).LastsLessThan(globalTimeoutInMsec + acceptableDeltaInMsec, TimeUnit.Milliseconds);
 
+            Check.That(smokeTestResult.IsSuccess).IsFalse();
+        }
+
+        [Test]
+        public async Task Return_false_success_when_smoke_tests_timeout_globally_without_NFluentLastLessThan()
+        {
+            var globalTimeoutInMsec = 2000;
+            var configuration = Stub.AConfiguration(globalTimeoutInMsec);
+            var smokeTestProvider = Stub.ASmokeTestProvider(new AlwaysPositiveSmokeTest(TimeSpan.FromSeconds(1.1)), new SmokeTestThrowingAnAccessViolationException(TimeSpan.FromSeconds(3.0)));
+
+            var controller = new SmokeController(configuration, null, smokeTestProvider);
+
+            SmokeTestSessionResult smokeTestResult = null;
+
+
+            var stopwatch = new Stopwatch();
+            smokeTestResult = await controller.RunSmokeTests();
+            stopwatch.Stop();
+
+            var acceptableDeltaInMsec = 700;
+            Check.That(stopwatch.Elapsed).IsLessThan(TimeSpan.FromMilliseconds(globalTimeoutInMsec + acceptableDeltaInMsec));
             Check.That(smokeTestResult.IsSuccess).IsFalse();
         }
 
