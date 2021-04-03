09b36575c60e4d36bd5a5d02d38f7d0d49704e98
tpierrain <thomas@pierrain.net>
2021-04-03 11:01:05 +0200

    Add TestDiscoverer and TestExecutor empty implementations.

---
+++ SmokeMe.TestAdapter/TestDiscoverer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+namespace SmokeMe.TestAdapter
+{
+    public class TestDiscoverer : ITestDiscoverer
+    {
+        public void DiscoverTests(IEnumerable<string> sources, IDiscoveryContext discoveryContext, IMessageLogger logger, ITestCaseDiscoverySink discoverySink)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
