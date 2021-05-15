6a9432145ea7780dce61b036e4155e00c852a3fd
tpierrain <thomas@pierrain.net>
2021-05-15 09:42:29 +0200

    Restore a completely different ICheckSmoke interface so that it explains to everyone what to do with v2.

---
+++ SmokeMe/ICheckSmoke.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SmokeMe
+{
+    /// <summary>
+    /// The ICheckSmoke interface is deprecated and MUST be replaced by SmokeTest abstract class. To do so, just replace all reference to ICheckSmoke with SmokeTest and add the 'override' keyword to your existing SmokeTestName, Description properties, but also to the Scenario() method which is now an abstract method.
+    /// </summary>
+    [Obsolete("The ICheckSmoke interface is deprecated and MUST be replaced by SmokeTest abstract class. To do so, just replace all reference to ICheckSmoke with SmokeTest and add the 'override' keyword to your existing SmokeTestName, Description properties, but also to the Scenario() method which is now an abstract method")]
+    public interface ICheckSmoke
+    {
+        /// <summary>
+        /// Breaking change: the ICheckSmoke interface is deprecated and MUST be replaced by SmokeTest abstract class. To do so, just replace all reference to ICheckSmoke with SmokeTest and add the 'override' keyword to your existing SmokeTestName, Description properties, but also to the Scenario() method which is now an abstract method.
+        /// </summary>
+        void WithTheV2MajorBreakingChangeOfSmokeMeYouMustReplaceAllYourPreviousReferenceToICheckSmokeInterfaceWithTheSmokeTestAbstractClass();
+    }
+}
